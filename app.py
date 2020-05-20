2b762a8e8ebd823e9527821ce56f0a845c79bc04
afratetlay <afratetlay@gmail.com>
2020-05-20 16:39:44 +0000

    working on my contact us page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipes'
+app.config["MONGO_URI"] = 'mongodb+srv://afra:Hellothere8@myfirstcluster-ji6z9.mongodb.net/recipes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
