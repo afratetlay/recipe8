6c840cf295c95c3cc5110cb5900f403703fc1fb3
afratetlay <afratetlay@gmail.com>
2020-05-21 15:30:02 +0000

    working on the template on my website

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import os
-from flask import Flask 
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'recipes'
-app.config["MONGO_URI"] = 'mongodb+srv://afra:Hellothere8@myfirstcluster-ji6z9.mongodb.net/recipes?retryWrites=true&w=majority'
-
-mongo = PyMongo(app)
-
 @app.route('/')
-def hello():
-    return 'Hello World'
+def tasks():
+    return render_template("tasks.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact-us.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
-
-    
+            port=int(os.environ.get('PORT')),
+            debug=True)
