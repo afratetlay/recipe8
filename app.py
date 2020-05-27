8968caa4aeced48be41ef05fb0db76e98bb902dc
afratetlay <afratetlay@gmail.com>
2020-05-27 20:11:17 +0000

    i have been working on my recipe page

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def tasks():
-    return render_template("tasks.html")
+def index():
+    return render_template("index.html")
+
+@app.route('/task')
+def task():
+    return render_template("task.html")
+
+@app.route('/recipes')
+def recipes():
+    return render_template("recipes.html")
 
 @app.route('/contact')
 def contact():
-    return render_template("contact-us.html")
+    return render_template("contact.html")
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
