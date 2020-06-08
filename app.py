092b5552f42fa9e7ee4bcffd710792628e8b2f76
afratetlay <afratetlay@gmail.com>
2020-06-08 14:19:41 +0000

    i am working on my app.py by connecting it to my MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'recipes'
+app.config["MONGO_URI"] = 'mongodb+srv://recipe8:Hellothere@myfirstcluster-ji6z9.mongodb.net/recipes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
